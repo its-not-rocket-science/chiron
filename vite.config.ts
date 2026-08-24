6c95a3588c731221d6b29d77e2d18b67d058577e
Paul Schleifer <paul_schleifer@hotmail.com>
2026-08-24 13:59:12 +0100

    Configure SvelteKit for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
-import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter()
-		})
+		sveltekit()
 	],
 	test: {
 		expect: { requireAssertions: true },
